78014fe9d98d735819b32a0dd3a0dd1185f483c8
Aymenbs22 <aymenbenslima0@gmail.com>
2021-09-30 04:14:10 -0700

    Write a C program that prints with puts a

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
 #include <stdio.h>
-
 /**
  * main - print string
  *
  * Return: 0
  */
-int  main(void)
-=======
-#!/bin/bash
 int main(void)
->>>>>>> 1bf9b5900592c3b65fb5318f4850c7c31daf2458
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
