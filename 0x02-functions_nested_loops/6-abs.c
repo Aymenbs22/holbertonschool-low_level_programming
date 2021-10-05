d606ec47b1e8a910a8378fd26e0267c4c864ac8f
Aymenbs22 <aymenbenslima0@gmail.com>
2021-10-05 09:21:14 -0700

    function that computes the absolute value of an integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
- * _islower - function that checks for lowercase character.
- * @c:first letter
+ * _abs - function that checks for lowercase character.
+ * @n:first letter
  * Return: 0
+ * @n:similar function
  */
 int _abs(int n)
 {
-   if (n < 0)
-    {
-            return (-n);
-    }
-    else
-    {
-            return (n);
-    }
-    return(0);
+	if (n < 0)
+{
+	return (-n);
+}
+	else
+{
+	return (n);
+}
+	return (0);
 }
