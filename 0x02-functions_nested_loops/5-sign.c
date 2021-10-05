a227e2234e3e9267cbf1581f1615ab7163f37dba
Aymenbs22 <aymenbenslima0@gmail.com>
2021-10-05 15:56:50 +0200

    function that prints the sign of a number

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - function that checks for lowercase character.
+ * @n:int n
+ * Return: 0
+ */
+int print_sign(int n);
+{
+	if (n > 0)
+{
+	_putchar('+');
+	return (1);
+}
+	else
+	(n < 0);
+{
+	_putchar('-');
+	return (-1);
+}
+	else if (n < 0)
+{
+	_putchar('0');
+	return (0);
+}
+
+}
