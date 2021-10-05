647eb43f61e05f3d50be599ca4c285c83a99c61e
Aymenbs22 <aymenbenslima0@gmail.com>
2021-10-05 10:12:31 -0700

    function that prints the last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+nclude "main.h"
+/**
+ * print_last_digit - function that checks for lowercase character.
+ *@n:first letter
+ * Return: 0
+ */
+int print_last_digit(int n)
+{
+	int a;
+
+	if (n < 0)
+	n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+	a = -a;
+
+	_putchar(a + '0');
+	return (a);
+}
