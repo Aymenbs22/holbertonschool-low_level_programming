5608b979ab1afc59b5fae49d13637bf3eb1f2092
Aymenbs22 <aymenbenslima0@gmail.com>
2021-10-06 14:44:51 +0200

    main.h.

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,15 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-int add(int, int);
-void print_to_98(int n);
-void times_table(void);
-#endif
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
