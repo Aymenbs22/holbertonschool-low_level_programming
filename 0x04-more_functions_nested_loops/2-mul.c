8b7a1991829df11d479ebacb269ad9dec06f4329
Aymenbs22 <aymenbenslima0@gmail.com>
2021-10-07 11:28:30 +0200

    function that multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* mul - tests function that prints if integer is positive or negative
+* Return: 0 or 1
+* @a: int
+* @b: int
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
