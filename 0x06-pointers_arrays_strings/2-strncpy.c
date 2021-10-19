901b5e06224a637abb475ee09cb7614665b79ee2
Aymenbs22 <aymenbenslima0@gmail.com>
2021-10-19 10:11:39 +0200

    function that copies a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *_strncpy - function that concatenates two strings
+ *@src: char
+ *@dest: char
+ * i - 0
+ * Return: dest
+ * @n : num bytes
+*/
+char *_strncpy(char *dest, char *src, int n);
+{
+int i = 0;
+for (src[i] != '\0' && i < n; i++)
+{
+dest[i] = src[i];
+}
+return(dest)
+}
+
