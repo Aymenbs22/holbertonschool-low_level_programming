0dcc4825d586e634203a643d03f19a9fa10e7b16
Aymenbs22 <90031228+Aymenbs22@users.noreply.github.com>
2021-10-19 14:51:50 +0200

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,18 +12,18 @@ while (cha[i])
 while (!(cha[i] >= 'a' && cha[i] <= 'z'))
 i++;
 if (cha[i - 1] == ' ' ||
-if cha[i - 1] == '\t' ||
-if cha[i - 1] == '\n' ||
-if cha[i - 1] == ';' ||
-if cha[i - 1] == ',' ||
-if cha[i - 1] == '.' ||
-if cha[i - 1] == '?' ||
-if cha[i - 1] == '!' ||
-if cha[i - 1] == '"' ||
-if cha[i - 1] == '(' ||
-if cha[i - 1] == ')' ||
-if cha[i - 1] == '{' ||
-if cha[i - 1] == '}' ||
+cha[i - 1] == '\t' ||
+cha[i - 1] == '\n' ||
+cha[i - 1] == ';' ||
+cha[i - 1] == ',' ||
+cha[i - 1] == '.' ||
+cha[i - 1] == '?' ||
+cha[i - 1] == '!' ||
+cha[i - 1] == '"' ||
+cha[i - 1] == '(' ||
+cha[i - 1] == ')' ||
+cha[i - 1] == '{' ||
+cha[i - 1] == '}' ||
 i == 0)
 cha[i] -= 32;
 i++;
