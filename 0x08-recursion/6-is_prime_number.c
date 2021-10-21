ebe64c6cbebcb6862f5992d1977e1fbf9cd72e2f
Aymenbs22 <aymenbenslima0@gmail.com>
2021-10-21 13:18:41 +0200

    function that returns 1 if the input integer

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * prm - function that prints a string in reverse
+ *@n: num!
+ *i: char
+ *@i: char
+ * Return: (prm(i + 1, n))
+*/
+int prm(int i, int n)
+
+{
+if (n < 2)
+return (0);
+if (n % i == 0)
+return (0);
+if (i > n / 2)
+return (1);
+return (prm(i + 1, n));
+}
+/**
+ * is_prime_number - function that returns 1
+ * @n: num
+ * Return: (is_it_prm(2, n))
+*/
+int is_prime_number(int n);
+{
+return (is_it_prm(2,));
+}
