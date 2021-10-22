9bed2162d34ed20abad272f8f4d4c2d35ca31cec
Aymenbs22 <aymenbenslima0@gmail.com>
2021-10-22 11:34:57 +0200

    program that prints its name, followed by a new line.*

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - function that prints a string in reverse
+ *@argc: int!
+ *@argv: int
+ * Return: 0
+*/
+int main(__attribute__((unused))int argc, char *argv[2])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
