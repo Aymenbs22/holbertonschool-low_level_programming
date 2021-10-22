988c919483104cc0e47173ced1b90b31dce740e5
Aymenbs22 <aymenbenslima0@gmail.com>
2021-10-22 12:37:45 +0200

    program that prints the number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - function that prints a string in reverse
+ *@argc: int!
+ *@argv: int
+ * Return: 0
+*/
+int main(__attribute__((unused))char *argv[], int argc)
+{
+printf("%d\n", argc - 1);
+return (0);
+}
