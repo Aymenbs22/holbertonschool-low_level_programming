e1a9f1587bccfc131d68b55564d07353b97cb79e
Aymenbs22 <aymenbenslima0@gmail.com>
2021-10-22 12:27:28 +0200

    main

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+int main (int argc, char *argv[]);
+int main(__attribute__((unused))int argc, char *argv[]);
+#endif
