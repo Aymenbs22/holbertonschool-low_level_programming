e6afc18484edcd2bad28ae0462d3f1a2da578eac
Aymenbs22 <aymenbenslima0@gmail.com>
2021-10-28 13:00:08 -0700

    function that allocates memory for an array, using malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -19,7 +19,7 @@ return (NULL);
 ptr = malloc(size * nmemb);
 if (ptr == NULL)
 return (NULL);
-for (i = 0; i < (size * nmemb);i++)
+for (i = 0; i < (size * nmemb); i++)
 ptr[i] = 0;
 j = ptr;
 return (j);
