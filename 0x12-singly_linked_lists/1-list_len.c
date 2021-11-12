4a2dbbee67de1f3d5590ffd4b9854809decb89bc
Aymenbs22 <aymenbenslima0@gmail.com>
2021-11-12 08:57:29 -0800

    list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * list_len - function that returns the number of elements in a linked list_t list
+ * list_len - function that returns the number of element
  * @h: pointer
  * Return: number of elements.
 */
@@ -11,7 +11,7 @@ size_t list_len(const list_t *h)
 unsigned int i;
 i = 0;
 	while (h != NULL)
-{	
+{
 		i = i + 1;
 		h = h->next;
 }
