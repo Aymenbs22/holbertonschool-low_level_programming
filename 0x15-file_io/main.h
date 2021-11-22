e389b79fe796dc617807b28e29b70cacd32701fd
Aymenbs22 <aymenbenslima0@gmail.com>
2021-11-22 13:48:27 -0800

    header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#define DATA_SIZE 1000
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
@@ -7,4 +8,5 @@
 #include <fcntl.h>
 #include <unistd.h>
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 #endif
