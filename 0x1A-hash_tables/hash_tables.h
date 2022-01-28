f6f812f0cb2425a8ff4cbbcf168149d1ceb1759a
aymenbs22 <aymenbenslima1@gmail.com>
2022-01-28 09:00:38 -0800

    header file

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -32,4 +32,5 @@ typedef struct hash_table_s
 	          hash_node_t **array;
 } hash_table_t;
 unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
 #endif
